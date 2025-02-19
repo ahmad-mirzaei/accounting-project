00b15081a4d77f05f0526178e31cb9be11aaa725
Ahmad <ahmadmirzaei.py@gmail.com>
2025-02-19 19:58:10 +0330

    edit

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-
+from django.core.exceptions import ValidationError
 
 class EmailLoginForm(AuthenticationForm):
     username = forms.EmailField(
         label = "ایمیل",
-        widget = forms.EmailInput(attrs = {"class": "form-control", "placeholder": "ایمیل را وارد کنید"})
+        widget = forms.EmailInput(attrs = {"class": "form-control", "placeholder": "ایمیل را وارد کنید"}),
+        error_messages = {
+            'required': 'لطفاً ایمیل خود را وارد کنید.',
+            'invalid': 'ایمیل وارد شده معتبر نیست.'
+        }
     )
+    password = forms.CharField(
+        label="رمز عبور",
+        widget=forms.PasswordInput(attrs={"class": "form-control", 'placeholder': 'رمز عبور خود را وارد کنید'})
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not User.objects.filter(email=email).exists():
+            raise ValidationError("ایمیل وارد شده وجود ندارد.")
+        return email
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomLoginView
+from .views import CustomLoginView, HomeView
 
 urlpatterns = [
+    path('', HomeView.as_view(), name = "home"),
     path('login/', CustomLoginView.as_view(), name = "login"),
 
 ]
