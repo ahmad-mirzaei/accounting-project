5d22b6e10ec3f07179a966bc6bea07a2609669eb
Ahmad <ahmadmirzaei.py@gmail.com>
2025-02-19 18:53:10 +0330

    add base.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from .forms import EmailLoginForm
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    form_class = EmailLoginForm
+    template_name = "accounts/login.html"
+
+    def get_success_url(self):
+        if self.request.user.is_superuser:
+            return reverse_lazy("admin:index")
+        return reverse_lazy("home")
