c28900a74a649dcb9008156f5b32332eee65e915
Ahmad <ahmadmirzaei.py@gmail.com>
2025-02-19 19:16:17 +0330

    add HomeView

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from .forms import EmailLoginForm
 # Create your views here.
+
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        context = {"user": request.user}
+        return render(request, "base.html", context)
+
 
 class CustomLoginView(LoginView):
     form_class = EmailLoginForm
