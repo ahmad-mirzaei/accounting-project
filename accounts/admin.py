3eecdc036d395e6df4468af52e3616c80e4f0de2
Ahmd Mirzaei <ahmadmirzaei.py@gmail.com>
2025-02-28 20:54:35 +0330

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
 
+# -----------------------------------------------------------------------
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
@@ -9,4 +10,6 @@ class CustomUserAdmin(admin.ModelAdmin):
     search_fields = ["email", ]
     list_editable = ["is_active", ]
     list_filter = ["is_active", "is_staff", "date_joined"]
+
+# -----------------------------------------------------------------------
 
