d9e1f2e0d4a3f2a25c7a83b1ecccbbd40df83b1e
Ahmd Mirzaei <ahmadmirzaei.py@gmail.com>
2025-02-28 20:55:01 +0330

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import CustomLoginView, HomeView, SignupView
 
+# -----------------------------------------------------------------------
+
 urlpatterns = [
     path('', HomeView.as_view(), name = "home"),
     path('signup/', SignupView.as_view(), name = "signup"),
